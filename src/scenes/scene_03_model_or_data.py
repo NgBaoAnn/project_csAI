544654b02a3738fe82a16df172be3d153cffedad
NgBaoAnn <baoan1real1@gmail.com>
2026-06-09 14:03:44 +0700

    Polish scene 03 model data focus animation

--- src/scenes/scene_03_model_or_data.py
+++ src/scenes/scene_03_model_or_data.py
@@ -55,10 +55,20 @@ class ModelOrDataScene(Scene):
             color=THEME_EMERALD,
             font_size=SIZE_CAPTION,
         ).to_edge(DOWN, buff=0.7)
+        bridge_arrow = Arrow(
+            model_box.get_right() + RIGHT * 0.15,
+            data_box.get_left() + LEFT * 0.15,
+            color=THEME_AMBER,
+            stroke_width=2.5,
+            buff=0.12,
+        )
+        bridge_dot = Dot(bridge_arrow.get_start(), color=THEME_AMBER, radius=0.055)
+        bridge_path = Line(bridge_arrow.get_start(), bridge_arrow.get_end())
 
         self.play(
             FadeIn(model_box, shift=RIGHT * 0.2),
             FadeIn(data_box, shift=LEFT * 0.2),
+            rate_func=smooth,
             run_time=TIME_NORMAL,
         )
         self.wait(9.0)
@@ -66,11 +76,20 @@ class ModelOrDataScene(Scene):
         self.wait(9.0)
         self.play(LaggedStart(*[FadeIn(item, shift=UP * 0.1) for item in model_items], lag_ratio=0.14), run_time=TIME_NORMAL)
         self.wait(11.0)
-        self.play(LaggedStart(*[FadeIn(item, shift=UP * 0.1) for item in data_items], lag_ratio=0.12), run_time=TIME_NORMAL)
+        self.play(GrowArrow(bridge_arrow), FadeIn(bridge_dot, scale=0.4), run_time=0.8)
+        self.play(MoveAlongPath(bridge_dot, bridge_path), run_time=1.0, rate_func=smooth)
+        self.play(FadeOut(bridge_dot), run_time=0.2)
+        self.play(
+            model_box.animate.set_stroke(opacity=0.45),
+            model_items.animate.set_opacity(0.55),
+            data_box.animate.set_fill(THEME_EMERALD, opacity=0.05).set_stroke(width=2.8),
+            LaggedStart(*[FadeIn(item, shift=UP * 0.1) for item in data_items], lag_ratio=0.12),
+            run_time=TIME_NORMAL,
+        )
         self.play(Circumscribe(data_box, color=THEME_EMERALD, buff=0.08), run_time=1.5)
         self.play(ApplyWave(data_items, direction=RIGHT, amplitude=0.12), run_time=1.5)
-        self.wait(13.0)
-        self.play(Create(focus), FadeIn(insight, shift=UP * 0.2), run_time=TIME_NORMAL)
+        self.wait(11.0)
+        self.play(FadeOut(bridge_arrow), Create(focus), FadeIn(insight, shift=UP * 0.2), run_time=TIME_NORMAL)
         self.wait(16.0)
 
         fade_out_all(self)
